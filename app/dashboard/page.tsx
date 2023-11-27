d57166361e884008146622101f8adc7ea841b5fb
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-27 13:26:18 +0700

    fix: rename heading

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,69 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import prisma from '@/lib/prima'
+import { currentUser } from '@clerk/nextjs'
+import { Suspense } from 'react'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import SadFace from '@/components/icons/SadFace'
+import CreateCollectionBtn from '@/components/CreateCollectionBtn'
+
+export default function Home() {
+  return (
+    <>
+      <Suspense fallback={<WelcomeMsgFallback />}>
+        <WelcomeMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading collections</div>}>
+        <CollectionList />
+      </Suspense>
+    </>
+  )
+}
+
+async function WelcomeMsg() {
+  const user = await currentUser()
+  if (!user) {
+    return <div>error</div>
+  }
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        Welcome, <br /> {user.firstName} {user.lastName}
+      </h1>
+    </div>
+  )
+}
+
+function WelcomeMsgFallback() {
+  return (
+    <div className="flex w-full">
+      <h1 className="text-4xl font-bold">
+        <Skeleton className="w-[150px] h-[36px]" />
+        <Skeleton className="w-[150px] h-[36px]" />
+      </h1>
+    </div>
+  )
+}
+
+async function CollectionList() {
+  const user = await currentUser()
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    },
+  })
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    )
+  }
+}
