ec2d2b396f0010006c5375a69cb6320bf16022b2
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-27 13:42:00 +0700

    style: theme configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import NavBar from '@/components/NavBar'
+import { Separator } from '@/components/ui/separator'
+import { ThemeProvider } from '@/providers/ThemeProvider'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +21,22 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html
+        lang="en"
+        className={cn(inter.className, 'dark')}
+        style={{ colorScheme: 'dark' }}
+      >
+        <body>
+          <ThemeProvider>
+            <div className="flex min-h-screen w-full flex-col items-center">
+              <NavBar />
+              <Separator />
+              <main className="flex flex-grow w-full justify-center items-center dark:bg-neutral-950">
+                {children}
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
